07ee5cc789a826bcff76c8cfd178be4d11834829
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-10 21:26:03 +0200

    add compose app location project uncopled

--- domain/src/main/java/toodoo/service/TodoService.kt
+++ domain/src/main/java/toodoo/service/TodoService.kt
@@ -4,7 +4,6 @@ import toodoo.model.Organization
 import toodoo.model.Todo
 
 interface TodoService {
-
     fun allTodos(): List<Todo>
     fun saveTodo(todo: Todo)
     fun allOrganizations(): List<Organization>
