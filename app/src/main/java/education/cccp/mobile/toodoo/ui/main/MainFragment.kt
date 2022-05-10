c5bfa2da1c7cdc3b66516316a925e77f0a26d698
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-10 02:14:25 +0200

    5

--- app/src/main/java/education/cccp/mobile/toodoo/ui/main/MainFragment.kt
+++ app/src/main/java/education/cccp/mobile/toodoo/ui/main/MainFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import education.cccp.mobile.toodoo.R
+import education.cccp.mobile.toodoo.R.layout
+import education.cccp.mobile.toodoo.R.layout.*
 
 class MainFragment : Fragment() {
 
@@ -19,9 +21,11 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+    ): View = inflater.inflate(
+        main_fragment,
+        container,
+        false
+    )
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
