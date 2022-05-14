4436612fd6e7f72045955186c5699490c38ad8a6
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-14 16:50:38 +0200

    fix code to run

--- app/src/androidTest/java/education/cccp/toodoo/mobile/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/education/cccp/toodoo/mobile/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("education.cccp.mobile.toodoo", appContext.packageName)
+        assertEquals("education.cccp.toodoo.mobile", appContext.packageName)
     }
 }
