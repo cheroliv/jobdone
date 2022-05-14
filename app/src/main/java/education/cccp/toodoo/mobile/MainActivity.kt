7972c36561e89469b63fc8ee5c3ae8f2ee88d8d2
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-14 16:23:41 +0200

    fix code to run

--- app/src/main/java/education/cccp/toodoo/mobile/MainActivity.kt
+++ app/src/main/java/education/cccp/toodoo/mobile/MainActivity.kt
@@ -2,8 +2,8 @@ package education.cccp.toodoo.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import education.cccp.mobile.toodoo.R.id.container
-import education.cccp.mobile.toodoo.R.layout.main_activity
+import education.cccp.toodoo.mobile.R.id.container
+import education.cccp.toodoo.mobile.R.layout.main_activity
 import education.cccp.toodoo.mobile.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/education/cccp/toodoo/mobile/ui/main/MainFragment.kt
+++ app/src/main/java/education/cccp/toodoo/mobile/ui/main/MainFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import education.cccp.mobile.toodoo.R
+import education.cccp.toodoo.mobile.R
 
 class MainFragment : Fragment() {
 
