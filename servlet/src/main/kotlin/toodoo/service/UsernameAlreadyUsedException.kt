1ead9d3fcb2acd14abd2c8ea592e2ab7fc256caa
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-14 13:00:12 +0200

    delete compose du projet

--- servlet/src/main/kotlin/toodoo/service/UsernameAlreadyUsedException.kt
+++
@@ -1,7 +0,0 @@
-package toodoo.service
-
-class UsernameAlreadyUsedException : RuntimeException("Login name already used!") {
-    companion object {
-        private const val serialVersionUID = 1L
-    }
-}
