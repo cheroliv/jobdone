0d44bbc4d8fab32a05062138861421640893a674
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-16 13:36:54 +0200

    premier test

--- app/src/main/java/education/cccp/toodoo/mobile/MainActivity.kt
+++ app/src/main/java/education/cccp/toodoo/mobile/MainActivity.kt
@@ -6,11 +6,13 @@ import education.cccp.toodoo.mobile.R.id.container
 import education.cccp.toodoo.mobile.R.layout.main_activity
 import education.cccp.toodoo.mobile.ui.main.MainFragment
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(main_activity)
+//        datas
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(container, MainFragment.newInstance())
